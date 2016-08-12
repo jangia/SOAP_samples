b6a52780320d5fe8f68c302a18f50c29f94eadcc
Jan Giacomelli <jan.giacomelli@result.si>
2016-08-12 08:56:48 +0200

    Test commit.

--- MySOAP.php
+++ MySOAP.php
@@ -11,6 +11,6 @@ die()*/;
 $url = 'http://10.10.12.30//result/My.wsdl';
 $request_data="jan";
 $client = new SoapClient($url,array('soap_version' => SOAP_1_2,'trace' => 1,'uri' => 'urn:mytargetns' ));
-
+//test
 $response = $client->__soapCall("MyOperation",array($request_data));
 var_dump($response);
